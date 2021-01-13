d7f485d692c25486dd56e0472f4c454ad8090c1d
Tanguy CHENIER <cerb4re@gmail.com>
2021-01-13 13:37:53 +0100

    Test Model

---
+++ backend/models/TestModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const TestSchema = mongoose.Schema({
+    userId: { type: String, required: true},
+    title: { type: String, required: true},
+    description: { type: String, required: true},
+    imageUrl: { type: String, required: true},
+
+    price: { type: Number, required: true},
+});
+
+module.exports = mongoose.model('Test', TestSchema);
