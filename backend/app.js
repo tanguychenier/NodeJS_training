61f0b5cb7714f59ce9b296b4c9268458c438afb5
Tanguy CHENIER <cerb4re@gmail.com>
2021-01-13 15:12:37 +0100

    Working delete

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,12 @@ app.put('/api/stuff/:id', (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Test.deleteOne({ _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Object has been deleted !' }))
+    .catch(error => res.status(404).json({ error }) );
+});
+
 app.get('/api/stuff/:id', (req, res, next) => {
     Test.findOne({ _id: req.params.id })
       .then(test => res.status(200).json(test))
