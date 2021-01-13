be807048b7716d6668c5b7103a3fa8a6c59ae36b
Tanguy CHENIER <tanguy.chenier@gmail.com>
2021-01-13 21:18:49 +0100

    correction about user instead of use, working login

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
-app.user('/api/auth', userRoutes);
+app.use('/api/auth', userRoutes);
  
 module.exports = app;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user');
 
-router.post('/signup', userController.signUp);
-router.post('/login', userController.login);
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
