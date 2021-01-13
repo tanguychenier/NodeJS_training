97614253d590a169c18788e86c791151625d6157
Tanguy CHENIER <tanguy.chenier@gmail.com>
2021-01-13 21:10:49 +0100

    User can now be created with bcypt on '/signup'

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,21 @@
-exports.signUp = (req, res, next) => {
+const bCrypt = require('bcrypt');
+const User = require('../models/User');
+const user = require('../models/User');
 
+exports.signUp = (req, res, next) => {
+    bCrypt.hash(req.body.password, 10)
+        .then(hash => {
+            
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+
+            user.save()
+                .then(() => res.status(201).json( { message: 'User has been created !' } ))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }) );
 };
 
 exports.login = (req, res, next) => {
