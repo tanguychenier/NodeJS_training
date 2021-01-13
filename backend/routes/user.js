badaa2e6bb8093297c3bb71ecfbdb3dfcc54fd5a
Tanguy CHENIER <tanguy.chenier@gmail.com>
2021-01-13 21:02:46 +0100

    working route for '/signup' && '/login'

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user');
+
+router.post('/signup', userController.signUp);
+router.post('/login', userController.login);
 
 module.exports = router;
