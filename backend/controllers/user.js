7506470d5b4115a71076474f8a13ac72644dd43b
Tanguy CHENIER <tanguy.chenier@gmail.com>
2021-01-13 21:43:28 +0100

    correction

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -21,19 +21,19 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
-          return res.status(401).json({ error: 'Cannot find this user !' });
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
-              return res.status(401).json({ error: 'Incorrect password !' });
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
             res.status(200).json({
               userId: user._id,
-              token: jsonWebToken.sign(
-                  { userId: user._id },
-                  'TEST_SECRET_TOKEN',
-                  { expiresIn: '24h' }
+              token: jwt.sign(
+                { userId: user._id },
+                'TEST_SECRET_TOKEN',
+                { expiresIn: '24h' }
               )
             });
           })
